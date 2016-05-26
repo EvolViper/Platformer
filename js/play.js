e31aa38dc27aa692c6f992c3ec5628315f3542a2
EvolViper <evol.viper@gmail.com>
2016-05-26 22:57:50 +0300

    вы

--- js/play.js
+++ js/play.js
@@ -113,7 +113,7 @@ var playState = {
 		enemy2.enemy.animations.play("move");
 
 		//Тест анимации
-		enemy3 = game.add.sprite(1200, 190, "monster_death");
+		enemy3 = game.add.sprite(1200, 170, "monster_death");
 		enemy3.animations.add("monster_die", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], 24, true);
 		enemy3.animations.play('monster_die');
 		
